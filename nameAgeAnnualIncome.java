a8443349d59cf9d5f8573ee18e47d01552b4d3e7
Nilima Manandhar <nilimamanandh@gmail.com>
2017-02-13 09:55:05 -0500

    a

---
+++ nameAgeAnnualIncome.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercises;
+
+/**
+ *
+ * @author Nilima M
+ */
+public class nameAgeAnnualIncome {
+    public static void main(String[] args)
+    {
+        String name="Joe Mahoney";
+        int age=26;
+        double annualPay=100000.0;
+        
+    }
+}
